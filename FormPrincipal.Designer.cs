1703f37084208457d2d037762fede64df0a605e7
Alejandro Ocampo Cisneros <ocampoalejandro.oc@gmail.com>
2020-12-22 08:30:12 -0600

    Se agregan comandos de saludo y despedida

--- FormPrincipal.Designer.cs
+++ FormPrincipal.Designer.cs
@@ -28,23 +28,145 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormPrincipal));
+            this.pictureAuto = new System.Windows.Forms.PictureBox();
+            this.pictureManu = new System.Windows.Forms.PictureBox();
+            this.pictureSimple2 = new System.Windows.Forms.PictureBox();
+            this.pictureSimpleOne = new System.Windows.Forms.PictureBox();
+            this.btnSign = new AsistenteDeReconocimiento.BotonCircular();
+            this.btnHelp = new AsistenteDeReconocimiento.BotonCircular();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureAuto)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureManu)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureSimple2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureSimpleOne)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureAuto
+            // 
+            this.pictureAuto.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.pictureAuto.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureAuto.Image = ((System.Drawing.Image)(resources.GetObject("pictureAuto.Image")));
+            this.pictureAuto.Location = new System.Drawing.Point(266, 46);
+            this.pictureAuto.Name = "pictureAuto";
+            this.pictureAuto.Size = new System.Drawing.Size(382, 565);
+            this.pictureAuto.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureAuto.TabIndex = 0;
+            this.pictureAuto.TabStop = false;
+            // 
+            // pictureManu
+            // 
+            this.pictureManu.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.pictureManu.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureManu.Image = ((System.Drawing.Image)(resources.GetObject("pictureManu.Image")));
+            this.pictureManu.Location = new System.Drawing.Point(774, 46);
+            this.pictureManu.Name = "pictureManu";
+            this.pictureManu.Size = new System.Drawing.Size(382, 565);
+            this.pictureManu.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureManu.TabIndex = 1;
+            this.pictureManu.TabStop = false;
+            // 
+            // pictureSimple2
+            // 
+            this.pictureSimple2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.pictureSimple2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureSimple2.Image = ((System.Drawing.Image)(resources.GetObject("pictureSimple2.Image")));
+            this.pictureSimple2.Location = new System.Drawing.Point(1121, 617);
+            this.pictureSimple2.Name = "pictureSimple2";
+            this.pictureSimple2.Size = new System.Drawing.Size(35, 31);
+            this.pictureSimple2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureSimple2.TabIndex = 5;
+            this.pictureSimple2.TabStop = false;
+            this.pictureSimple2.Visible = false;
+            // 
+            // pictureSimpleOne
+            // 
+            this.pictureSimpleOne.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.pictureSimpleOne.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureSimpleOne.Image = ((System.Drawing.Image)(resources.GetObject("pictureSimpleOne.Image")));
+            this.pictureSimpleOne.Location = new System.Drawing.Point(616, 617);
+            this.pictureSimpleOne.Name = "pictureSimpleOne";
+            this.pictureSimpleOne.Size = new System.Drawing.Size(32, 31);
+            this.pictureSimpleOne.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureSimpleOne.TabIndex = 6;
+            this.pictureSimpleOne.TabStop = false;
+            this.pictureSimpleOne.Visible = false;
+            // 
+            // btnSign
+            // 
+            this.btnSign.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSign.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnSign.BackgroundImage")));
+            this.btnSign.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnSign.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSign.FlatAppearance.BorderSize = 0;
+            this.btnSign.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSign.Location = new System.Drawing.Point(1344, 587);
+            this.btnSign.Name = "btnSign";
+            this.btnSign.Size = new System.Drawing.Size(39, 38);
+            this.btnSign.TabIndex = 3;
+            this.btnSign.UseVisualStyleBackColor = true;
+            this.btnSign.Click += new System.EventHandler(this.btnSign_Click);
+            // 
+            // btnHelp
+            // 
+            this.btnHelp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnHelp.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnHelp.BackgroundImage")));
+            this.btnHelp.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnHelp.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnHelp.FlatAppearance.BorderSize = 0;
+            this.btnHelp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHelp.Location = new System.Drawing.Point(1344, 631);
+            this.btnHelp.Name = "btnHelp";
+            this.btnHelp.Size = new System.Drawing.Size(39, 38);
+            this.btnHelp.TabIndex = 2;
+            this.btnHelp.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(172, 625);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(352, 23);
+            this.textBox1.TabIndex = 7;
             // 
             // FormPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(18)))));
-            this.ClientSize = new System.Drawing.Size(1540, 845);
+            this.ClientSize = new System.Drawing.Size(1386, 681);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureSimpleOne);
+            this.Controls.Add(this.pictureSimple2);
+            this.Controls.Add(this.btnSign);
+            this.Controls.Add(this.btnHelp);
+            this.Controls.Add(this.pictureManu);
+            this.Controls.Add(this.pictureAuto);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "FormPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Asistente de Reconocimiento";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.FormPrincipal_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureAuto)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureManu)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureSimple2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureSimpleOne)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureAuto;
+        private System.Windows.Forms.PictureBox pictureManu;
+        private BotonCircular btnHelp;
+        private BotonCircular btnSign;
+        private System.Windows.Forms.PictureBox pictureSimple2;
+        private System.Windows.Forms.PictureBox pictureSimpleOne;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- FormPrincipal.cs
+++ FormPrincipal.cs
@@ -1,20 +1,155 @@
-﻿using System;
+﻿
+//System.Speech.Synthesis funciones que permiten hablar al usuario
+//System.Speech.Recognoized funciones que permiten escuchar al usuario
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.Speech.Recognition;
+
 
 namespace AsistenteDeReconocimiento
 {
     public partial class FormPrincipal : Form
     {
+        #region Variables globales
+        string word;
+        int aleatorio;
+        Random random = new Random();
+
+        #region orejadelPrograma
+        SpeechRecognitionEngine oreja = new SpeechRecognitionEngine();
+        #endregion
+        #region bocadelPrograma
+        SpeechSynthesizer boca = new SpeechSynthesizer();
+        List<VoiceInfo> voiceInfos = new List<VoiceInfo>();
+        #endregion
+
+        #endregion
+
+        #region Inicialización
         public FormPrincipal()
         {
             InitializeComponent();
+            foreach (InstalledVoice voz in boca.GetInstalledVoices())
+            {
+                voiceInfos.Add(voz.VoiceInfo);
+            }
+            boca.Volume = 100;
+            boca.Rate = 2;
+            oreja.SetInputToDefaultAudioDevice();
+            oreja.LoadGrammar(new DictationGrammar());
+            oreja.SpeechRecognized += Lenguaje;
+            oreja.RecognizeAsync(RecognizeMode.Multiple);            
         }
+        #endregion|
+
+        #region MétodoDetection
+        private void Lenguaje(object sender, SpeechRecognizedEventArgs e)
+        {
+            
+            textBox1.Text = e.Result.Text;           
+            if (e.Result.Text == "hola" || e.Result.Text == "ola" || e.Result.Text == "o la")
+            {
+                boca.Rate = 2;
+                boca.SpeakAsync("Hola jefe, ¿Cómo va todo?");
+            }
+            else if (e.Result.Text == "bien" || e.Result.Text == "estoy bien")
+            {
+                boca.SpeakAsync("Me alegra oir eso");
+            }
+            else if (e.Result.Text == "mal" || e.Result.Text == "estoy mal")
+            {
+                boca.SpeakAsync("¿por qué?");
+            }
+            else if (e.Result.Text == "activa señas" || e.Result.Text == "señas")
+            {
+                boca.SpeakAsync("Activando modo de accesibilidad");
+                pictureSimpleOne.Visible = true;
+                pictureSimple2.Visible = true;
+            }
+            else if (e.Result.Text == "adiós" || e.Result.Text == "a Dios" || e.Result.Text == "el avion" || e.Result.Text=="avión se") 
+            {
+                GeneraRespuestaSincrona("adios", "Nos vemos", "Cerrando", "Hasta luego, jefe", "Cuidate", "Siempre es un placer ayudarlo");
+                this.Close();
+
+            }
+        }
+        #endregion
+
+        #region Accesibilidad
+        private void btnSign_Click(object sender, EventArgs e)
+        {
+            if(pictureSimple2.Visible==true)
+            {
+                pictureSimpleOne.Visible = false;
+                pictureSimple2.Visible = false;
+            }
+            else
+            {
+                pictureSimpleOne.Visible = true;
+                pictureSimple2.Visible = true;
+            }
+            
+        }
+        #endregion
+
+        #region LoadPrincipal
+        private void FormPrincipal_Load(object sender, EventArgs e)
+        {
+            GeneraRespuestaAsincrona("hola", "hola", "hola estoy lista para trabajar", "Saludos jefe", "Me alegra verte","hola, No has cambiado nada");
+        }
+        #endregion
+
+        #region GenerarRespuestaSincrona5
+        private void GeneraRespuestaSincrona(string frase, string uno, string dos, string tres, string cuatro, string cinco)
+        {
+            aleatorio = random.Next(1, 5);
+            switch (aleatorio)
+            {
+                case 1:
+                    boca.Speak(uno.ToString());
+                    break;
+                case 2:
+                    boca.Speak(dos.ToString());
+                    break;
+                case 3:
+                    boca.Speak(tres.ToString());
+                    break;
+                case 4:
+                    boca.Speak(cuatro.ToString());
+                    break;
+                case 5:
+                    boca.Speak(cinco.ToString());
+                    break;
+            }
+        }
+        #endregion
+
+        #region GenerarRespuestaAsincrona5
+        private void GeneraRespuestaAsincrona(string frase, string uno, string dos, string tres, string cuatro, string cinco)
+        {
+            aleatorio = random.Next(1, 5);
+            switch (aleatorio)
+            {
+                case 1:
+                    boca.SpeakAsync(uno.ToString());
+                    break;
+                case 2:
+                    boca.SpeakAsync(dos.ToString());
+                    break;
+                case 3:
+                    boca.SpeakAsync(tres.ToString());
+                    break;
+                case 4:
+                    boca.SpeakAsync(cuatro.ToString());
+                    break;
+                case 5:
+                    boca.SpeakAsync(cinco.ToString());
+                    break;
+            }
+        }
+        #endregion
+
     }
 }
